27ce0f75d5859393e7412c7ba125ae684ec848f5
example@gmail.com <s.griszko@gmail.com>
2021-06-14 20:17:07 +0200

    carousel implimented

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ $(function () {
   $('.slider').slick({
     dots: false,
     infinite: true,
-    speed: 300,
+    arrows: true,
+    speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     prevArrow:
-      '<span style="font-size: 48px; color: Dodgerblue; class="priv_arrow"><i class="fas fa-chevron-circle-left"></i></span>',
+      '<div class="left-arrow"><span class="prev_arrow"><img src="img/arrow-left.svg" alt=""></span></div>',
     nextArrow:
-      '<span class="next_arrow"><img src="img/ears.svg" alt=""></span>',
+      '<div class="right-arrow"><span class="next_arrow"><img src="img/arrow-right.svg" alt=""></span></div>',
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
-          dots: true,
+          dots: false,
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
         },
       },
       {
