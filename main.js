a702add4fb855d1480feb5da3565150385bc8c0b
trader TGN <trader@tcn.local>
2021-06-14 15:14:40 +0200

    carousel

--- main.js
+++ main.js
@@ -48,24 +48,24 @@ prevBtn.addEventListener('click', () => {
   carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 });
 
-var slideIndex = 1;
-showDivs(slideIndex);
+// var slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs((slideIndex += n));
-}
+// function plusDivs(n) {
+//   showDivs((slideIndex += n));
+// }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName('mySlides');
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'flex';
-  }
-  x[slideIndex - 1].style.display = 'block';
-}
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName('mySlides');
+//   if (n > x.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = x.length;
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = 'flex';
+//   }
+//   x[slideIndex - 1].style.display = 'block';
+// }
